cdfc428496dd2d06fcd9c8da2577da3977df4384
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2014-05-15 23:57:08 +0200

    Updated systemstats, and made a symlink to it.

--- systemstats.sh
+++ systemstats.sh
@@ -2,23 +2,23 @@
 # Server Status Script
 # Version 0.1.3 m
 # Updated: July 26th 2011 m 
-CPUTIME=$(ps -eo pcpu | awk 'NR>1' | awk '{tot=tot+$1} END {print tot}')                                                                                                                                                                     
-CPUCORES=$(cat /proc/cpuinfo | grep -c processor)                                                                                                                                                                                            
-UP=$(echo `uptime` | awk '{ print $3 " " $4 }')
+CPUTIME=$(ps -eo pcpu | awk 'NR>1' | awk '{tot=tot+$1} END {print tot}')
+CPUCORES=$(cat /proc/cpuinfo | grep -c processor)
+UP=$(uptime | awk '{ print $3 " " $4 }' | sed 's/,//g')
 echo "
-===================== SYSTEM INFORMATION ==================                                                                                                                                                                                  
-- Date                      = `date`                                                                                                                                                                                                         
-- Server Name               = `hostname`                                                                                                                                                                                                     
-- Public IP                 = `dig +short myip.opendns.com @resolver1.opendns.com`                                                                                                                                                           
-- OS Version                = `lsb_release -d |sed 's/Description:\t//g'`                                                                                                                                                                    
-- Load Averages             = `cat /proc/loadavg`                                                                                                                                                                                            
-- System Uptime             = `echo $UP`                                                                                                                                                                                                     
-- Platform Data             = `uname -orpi`                                                                                                                                                                                                  
-- CPU Usage (average)       = `echo $CPUTIME / $CPUCORES | bc`%                                                                                                                                                                              
-- Memory free (real)        = `free -m | head -n 2 | tail -n 1 | awk {'print $4'}` Mb                                                                                                                                                        
-- Memory free (cache)       = `free -m | head -n 3 | tail -n 1 | awk {'print $3'}` Mb                                                                                                                                                        
-- Swap in use               = `free -m | tail -n 1 | awk {'print $3'}` Mb                                                                                                                                                                    
-"                                                                                                                                                                                                                                            
-                                                                                                                                                                                                                                             
-echo "===================== DISK USAGE =========================="                                                                                                                                                                           
-df -h | egrep '(Filesystem)|(/dev/sd)'
+===================== SYSTEM INFORMATION ==================
+- Date                      = $(date)
+- Server Name               = $(hostname)
+- Public IP                 = $(dig +short myip.opendns.com @resolver1.opendns.com)
+- OS Version                = $(lsb_release -d |cut -f2-)
+- Load Averages             = $(cat /proc/loadavg | awk '{print $1" "$2" "$3}')
+- System Uptime             = $(echo $UP)
+- Platform Data             = $(uname -ri)
+- CPU Usage (average)       = $(echo $CPUTIME / $CPUCORES | bc)%
+- Memory free (real)        = $(free -m | head -n 2 | tail -n 1 | awk {'print $4'}) Mb
+- Memory free (cache)       = $(free -m | head -n 3 | tail -n 1 | awk {'print $3'}) Mb
+- Memory used               = $(free -m | head -n 2 | tail -n 1 | awk '{print $3}') Mb
+- Swap in use               = $(free -m | tail -n 1 | awk {'print $3'}) Mb
+"
+echo "===================== DISK USAGE =========================="
+df -hT | egrep '(Filesystem)|(/dev/sd)|(corellian-corvette)|(storage)'
