9b272337e07220df32272154dba6c77a7ecebac5
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2014-10-28 12:26:36 +0100

    Removed mount statements

--- standby.sh
+++ standby.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 export DISPLAY=:0
 /home/lordievader/scripts/mount.sh -u
-#sudo truecrypt -d /media/Documents
-#sudo truecrypt -d /media/Photos
 
 qdbus org.freedesktop.ScreenSaver /ScreenSaver Lock
 sleep 1
@@ -10,7 +8,6 @@ sudo pm-suspend
 sleep 1
 xrandr --output LVDS --auto
 /home/lordievader/scripts/gamma.sh LVDS --set
-/home/lordievader/scripts/mount.sh -t
 
 counter=0
 while ! ping -q -c1 8.8.8.8 && [ $counter -lt 5 ] ; do
