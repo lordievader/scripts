a37e568268605ec64f6a1e2e60aee2d6ef4a6a43
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2014-10-28 14:08:17 +0100

    Added a socket to wallpaper
    The wallpaper can be changed by sending "change" as a UDP frame to
    127.0.0.1 port 1025.

--- wallpaper.py
+++ wallpaper.py
@@ -3,14 +3,18 @@
 import random
 import os
 import signal
+import socket
+import select
 import subprocess
 import sys
 import time
 
-if len(sys.argv) < 2:
+def too_few_arguments(num):
 
-  print("Too few arguments")
-  sys.exit()
+  if len(sys.argv) < num+1:
+
+    print("Too few arguments")
+    sys.exit()
 
 def kill_others():
 
@@ -24,16 +28,31 @@ def kill_others():
 
 def load_arguments():
 
-    image_folders = []
-    for i, folder in enumerate(sys.argv):
+    too_few_arguments(1)
+    mode = 'start'
+    try:
 
-        if i == 1:
+      int(sys.argv[1])
+    except:
 
-            time = folder
-        else:
+      mode = 'change'
+    if mode == 'start':
 
-            image_folders.append(folder)
-    return (time, image_folders)
+      too_few_arguments(2)
+      image_folders = []
+      for i, folder in enumerate(sys.argv):
+
+          if i == 1:
+
+              delay = int(folder)
+          else:
+
+              image_folders.append(folder)
+    else:
+
+      delay = 0
+      image_folders = ''
+    return (mode, delay, image_folders)
 
 def generate_image_pool(image_folders):
 
@@ -80,20 +99,90 @@ def set_lockscreen(image):
     print(command)
     subprocess.getoutput(command)
 
-        
+def handler():
+
+  global sock
+  sock  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  sock.bind(('127.0.0.1', 1025))
+  sock.setblocking(0)
+
+def receive():
+
+  global sock
+  result = select.select([sock],[],[], 5)
+  if result[0]:
+
+    data, addr = result[0][0].recvfrom(1024)
+    try:
+
+      data_string = str(data,'utf-8').replace('\n','')
+      if data_string == 'change':
+
+        print("Changing")
+        global image_pool
+        image = random_image(image_pool)
+        set_background(image)
+        global time_change
+        time_change = 0
+    except:
+
+      pass
+
+def send(data):
+
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  sock.sendto(data, ('127.0.0.1', 1025))
+
+def change():
+
+  running = False
+  pid = os.getpid()
+  others = subprocess.getoutput("pgrep wallpaper.py").split("\n")
+  for other in others:
+
+      if other != '' and int(other) != int(pid):
+
+        running = True
+        break
+  if running == False:
+
+    print("Wallpaper manager not running please start it!")
+    sys.exit()
+  data = bytes("change", 'utf-8')
+  send(data)
 
 def main():
 
-    kill_others()
-    delay, image_folders = load_arguments()
-    image_pool = generate_image_pool(image_folders)
-    image = random_image(image_pool)
-    set_background(image)
-    while True:
+    mode, delay, image_folders = load_arguments()
+    if mode == 'start':
 
-        time.sleep(int(delay))
-        image = random_image(image_pool)
-        set_background(image)
+      kill_others()
+      global image_pool
+      image_pool = generate_image_pool(image_folders)
+      image = random_image(image_pool)
+      set_background(image)
+
+      global wait_time
+      wait_time = int(delay/10)
+      handler()
+
+      global time_change
+      time_change = 0
+      while True:
+
+          if time_change == 0:
+
+            time_change = time.time() + int(delay)
+          if time.time() > time_change:
+
+            image = random_image(image_pool)
+            set_background(image)
+          else:
+
+            receive()
+    else:
+
+      change()
 
 if __name__ == "__main__":
 
