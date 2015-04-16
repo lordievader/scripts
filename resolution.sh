c317c5780f76008dde434e164c5ec99cc191530d
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2015-04-16 09:42:28 +0200

    Resolution.sh output prettified

--- resolution.sh
+++ resolution.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 CURDIR=$(pwd)
 for item in $CURDIR/*; do
-  RESOLUTION=$(ffmpeg -i "$item" 2>&1|grep Video | head -n1 |sed -e 's,.* \([0-9]\+x[0-9]\+\)[ \,].*,\1,')
-  echo $RESOLUTION -- $item
-done
+  DATA=$(ffmpeg -i "$item" 2>&1|grep "from\|Video:")
+  echo $DATA |sed -e "s,.*'\(/.*\)'\:\ .*\ \([0-9]\+\)x\([0-9]\+\)[ \,].*,\1-\2-\3,g"|awk -F '-' '{
+    file = $1;
+    width = $2;
+    height = $3;
+    if ( width < 1280)
+    {
+      printf "%4s x %4u %-100s\n", width, height, file;
+    }
+  }'
+done|sort -n
