59d61cf2cbd9606ec8b19f971ab450222cfdc1c6
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2016-01-23 12:30:08 +0100

    Better handling of spaces
    The filesize script didn't handle spaces in filenames properly.

--- filesize.sh
+++ filesize.sh
@@ -4,40 +4,11 @@ if [[ $DIR != "" ]]; then
     DIR="$DIR/"
 fi
 
-#output=$(for item in * .* ; do
-#    if [[ $item != '.' ]] && [[ $item != '..' ]]; then
-#        du -xks $DIR"$item"
-#    fi
-#done | sort -nr | awk '{
-#    Size = $1;
-#    Directory = $2;
-#    if ( Size > 1024 && Size <= 1048576 ) {
-#        Size_MB = Size / 1024;
-#        printf "%5u MB %-100s;", Size_MB, Directory;
-#    }
-#    else if ( Size > 1048576 ) {
-#        Size_GB = Size / 1048576;
-#        printf "%5u GB %-100s;\n", Size_GB, Directory;
-#    }
-#    else {
-#        printf "%5u kB %-100s;\n", Size, Directory;
-#    }
-#
-#  }')
-#count=$(echo $output|sed 's,;,\n,g'|wc -l)
-#if [[ $count -gt $(tput lines) ]]; then
-#    echo $output|sed 's,;,\n,g'|less
-#else
-#    #echo $output|sed 's,;,\n,g'
-#    echo $output
-#fi
-
-
 for item in * .* ; do
     if [[ $item != '.' ]] && [[ $item != '..' ]]; then
         du -xks $DIR"$item"
     fi
-done | sort -nr | awk '{
+done | sort -nr | sed -e 's|\([0-9]\+\)\s*\(.*\)|\1;\2|g' -e 's|\s\+|\*|g' -e 's|;|\ |g'| awk '{
     Size = $1;
     Directory = $2;
     if ( Size > 1024 && Size <= 1048576 ) {
@@ -52,4 +23,4 @@ done | sort -nr | awk '{
         printf "%5u kB %-50s\n", Size, Directory;
     }
 
-  }'|less -c
+  }'|sed 's|\*|\ |g'|less -c
