a6b11fbed618e68a2d79074e2fa2ff2bb120adb2
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2016-07-06 17:27:26 +0200

    Update dirvish script

--- dirvish.sh
+++ dirvish.sh
@@ -1,4 +1,70 @@
 #!/bin/bash
+function checkLV {
+  available=true
+  for item in $(lvs -o lv_name,lv_attr --noheadings|grep backup|sed -e 's,^\ \+,,g' -e 's,\ \+,+,g'); do
+    name=$(echo $item|sed 's,+.*$,,')
+    state=$(echo $item|sed 's,^.*+,,')
+    if [[ "$state" != 'rwi-a-r--' ]]; then
+      available=false
+      echo "$name is not available"
+    fi
+  done
+  return available
+}
+
+function mountBackup {
+  echo -n "Mounting backup... "
+  mount /backup/corellian-corvette
+  mount /backup/vm
+  echo "done!"
+}
+
+function umountBackup {
+  echo -n "Unmounting backup... "
+  umount /backup/corellian-corvette
+  if [[ $? != 0 ]]; then
+    echo
+    lsof /backup/corellian-corvette
+  fi
+  umount /backup/vm
+  if [[ $? != 0 ]]; then
+    echo
+    lsof /backup/vm
+  fi
+  echo "done!"
+}
+
+function dirvishExpire {
+  echo "Running expire"
+  dirvish-expire
+  echo "Done"
+}
+
+function dirvishRun {
+  echo "Starting dirvish run"
+  dirvish-runall
+  echo "Done"
+}
+
+function stats {
+    echo "Stats"
+    df -h|grep -e Filesystem -e backup
+    echo "Done"
+}
+
+if [[ checkLV == false ]]; then
+  echo "One or more LogicalVolumes are not available."
+  exit 1
+else
+  mountBackup
+  dirvishExpire
+  dirvishRun
+  stats
+  sleep 60
+  umountBackup
+fi
+
+exit 0
 BACKUP_MOUNTED=$(cryptsetup status /dev/mapper/backup|head -n1|awk '{print $3}')
 if [ $BACKUP_MOUNTED == "active" ]; then
   echo "Mounting backup"
@@ -14,7 +80,7 @@ if [ $BACKUP_MOUNTED == "active" ]; then
   echo "Done"
   
   echo "Stats"
-  df -h|grep -e Filesystem -e backup
+  echo $(df -h|grep -e Filesystem -e backup)
 
   echo "Unmounting backup"
   umount /backup/corellian-corvette
